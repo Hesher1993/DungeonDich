6c5b723f1d4e918ae7438ba4399302d424282670
Hesher1993 <Daredevillll49@yandex.ru>
2020-11-28 13:11:11 +0300

    Homework 5

---
+++ core/src/ru/geekbrains/dungeon/game/UnitController.java
@@ -0,0 +1,100 @@
+package ru.geekbrains.dungeon.game;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitController {
+    private GameController gc;
+    private MonsterController monsterController;
+    private Hero hero;
+    private Unit currentUnit;
+    private int index;
+    private List<Unit> allUnits;
+    private int round;
+
+    public MonsterController getMonsterController() {
+        return monsterController;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public boolean isItMyTurn(Unit unit) {
+        return currentUnit == unit;
+    }
+
+    public boolean isCellFree(int cellX, int cellY) {
+        for (Unit u : allUnits) {
+            if (u.getCellX() == cellX && u.getCellY() == cellY) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public UnitController(GameController gc) {
+        this.gc = gc;
+        this.hero = new Hero(gc);
+        this.monsterController = new MonsterController(gc);
+    }
+
+    public void init() {
+        this.monsterController.activate(5, 5);
+        this.monsterController.activate(9, 5);
+        this.index = -1;
+        this.allUnits = new ArrayList<>();
+        this.allUnits.add(hero);
+        this.allUnits.addAll(monsterController.getActiveList());
+        this.round = 0;
+        this.nextTurn();
+    }
+
+    public void nextTurn() {
+        index++;
+        if (index >= allUnits.size()) {
+            index = 0;
+        }
+        currentUnit = allUnits.get(index);
+        roundUp(currentUnit);
+        currentUnit.startTurn();
+    }
+
+    public void roundUp(Unit currentUnit) {
+        if (currentUnit.equals(hero)) {
+            round++;
+            if ((float) round % 3 == 0.0f) {
+                this.allUnits.add(this.monsterController.activate(9, 5));
+            }
+        }
+    }
+
+    public void render(SpriteBatch batch, BitmapFont font18) {
+        hero.render(batch, font18);
+        monsterController.render(batch, font18);
+    }
+
+    public void update(float dt) {
+        hero.update(dt);
+        monsterController.update(dt);
+
+        if (!currentUnit.isActive() || currentUnit.getTurns() == 0) {
+            nextTurn();
+        }
+    }
+
+    public void removeUnitAfterDeath(Unit unit) {
+        int unitIndex = allUnits.indexOf(unit);
+        allUnits.remove(unit);
+        if (unitIndex <= index) {
+            index--;
+        }
+    }
+
+    public int getRound() {
+        return round;
+    }
+}
